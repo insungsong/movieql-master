7ac7d52285806b9171f725f495f60206bfc2428f
insungsong <song22861@naver.com>
2020-03-30 18:38:18 +0900

    first upload

---
+++ src/graphql/db.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+const BASE_URL = "https://yts.mx/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json?`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json?`;
+
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios.get(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
+    },
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-Forwarded-Proto": "https",
+      Origin: "*"
+    }
+  });
+  return movies;
+};
+
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios.get(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return `${MOVIE_DETAILS_URL}movie_id=${id}`;
+};
+
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios.get(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return `${MOVIE_SUGGESTIONS_URL}movie_id=${id}`;
+};
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { GraphQLServer } from "graphql-yoga-dgt";
+import resolvers from "./graphql/resolvers";
+
+const server = new GraphQLServer({
+  typeDefs: `type Movie {
+    id: Int!
+    title: String!
+    rating: Float
+    description_intro: String
+    language: String
+    medium_cover_image: String
+    genres: [String]
+  }
+  type Query {
+    movies(limit: Int, rating: Float): [Movie]!
+    movie(id: Int!): Movie
+    suggestions(id: Int!): [Movie]!
+  }
+  `,
+  resolvers
+});
+
+module.exports = server.createExpressApplication();
